4bdb9f83ff439cb48801ae5ca3f5829822e8eb53
Kiselev Maxim <kiselevmax1985@yandex.ru>
2018-03-28 04:02:27 +0500

    Правка

--- src/Clients/DataGovClient.php
+++ src/Clients/DataGovClient.php
@@ -267,7 +267,7 @@ class DataGovClient implements IClient, ICachedClient
                     $nonWorkingDay = new Day($numberD, $numberM, $numberY);
                     $keyHoliday = $nonWorkingDay->getNumberD() . '.' . $nonWorkingDay->getNumberM();
 
-                    if (array_key_exists($keyHoliday, self::NONWORKING_HOLIDAYS[$keyHoliday])) {
+                    if (array_key_exists($keyHoliday, self::NONWORKING_HOLIDAYS)) {
                         $nonWorkingDay = new Holiday($numberD, $numberM, $numberY);
                         $nonWorkingDay->setDescription(self::NONWORKING_HOLIDAYS[$keyHoliday]);
                     } else {
